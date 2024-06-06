a30ea9a53be3cab0f76f83d7470daf0cd17c3fe5
pukpuk555 <jester2540@gmail.com>
2024-06-06 15:27:33 +0700

    basic Axis complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import PokemonBasicFetch from "./components/PokemonBasicFetch";
-// import PokemonBasicFetchAxios from "./components/PokemonBasicFetchAxios";
+// import PokemonBasicFetch from "./components/PokemonBasicFetch";
+import PokemonBasicFetchAxios from "./components/PokemonBasicFetchAxios";
 // import PokemonFetch from "./components/PokemonFetch";
 // import PokemonFetchAxios from "./components/PokemonFetchAxios";
 // import PokemonSearchFetch from "./components/PokemonSearchFetch";
@@ -8,10 +8,10 @@ const App = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       {/* Fetch API */}
-      <PokemonBasicFetch />
+      {/* <PokemonBasicFetch /> */}
       {/* <PokemonFetch /> */}
       {/* Axios */}
-      {/* <PokemonBasicFetchAxios /> */}
+      <PokemonBasicFetchAxios />
       {/* <PokemonFetchAxios /> */}
       {/* Bonus Code Example */}
       {/* <PokemonSearchFetch /> */}
